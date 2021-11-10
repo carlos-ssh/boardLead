16001ab0fc7c105cdb085794a4ef649fdf469713
Carlos-SSH <carlos.anezeh@me.com>
2021-11-10 16:56:03 -0600

    Add basic structure.

---
+++ src/components/form.js
@@ -0,0 +1,7 @@
+const displayForm = () => {
+  const form = document.getElementById(form);
+  const input = document.getElementById(input);
+  form.style.display = "block";
+}
+
+export default displayForm;
