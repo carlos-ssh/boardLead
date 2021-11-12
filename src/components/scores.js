225dbf8a69cbabd1c90aee8b5618db75ec302e40
Carlos-SSH <carlos.anezeh@me.com>
2021-11-12 15:07:02 -0600

    Add functions to add and get data from api.

--- src/components/scores.js
+++ src/components/scores.js
@@ -1,27 +1,16 @@
-const scores = [
-  {
-    id: 1,
-    name: 'Вася',
-    score: 10,
-  },
-  {
-    id: 2,
-    name: 'Петя',
-    score: 20,
-  },
-  {
-    id: 3,
-    name: 'Маша',
-    score: 30,
-  },
-  {
-    id: 4,
-    name: 'Даша',
-    score: 40,
-  }
-];
+import axios from 'axios';
 
-const getScore = () => scores;
-const addScore = (score) => scores.push(score);
+const gameId = 'Zl4d7IVkemOTTVg2fUdz';
+const apiURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/`;
+
+const getScore = async () => {
+  const response = await axios.get(`${apiURL}/games/${gameId}/scores`);
+  return response;
+};
+
+const addScore = async (score) => {
+  const response = await axios.post(`${apiURL}/games/${gameId}/scores`, game);
+  return response;
+}
 
 export { getScore, addScore };
