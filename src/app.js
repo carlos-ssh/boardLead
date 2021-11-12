22b90357df72b069c323f6e048eb38d528243625
Carlos-SSH <carlos.anezeh@me.com>
2021-11-12 09:59:46 -0600

    Add a function to create a new form.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 import './style.css';
+import displayForm from './components/form';
+
+const app = () => {
+  displayForm();
+}
+
+app();
--- src/components/form.js
+++ src/components/form.js
@@ -1,7 +1,32 @@
 const displayForm = () => {
-  const form = document.getElementById(form);
-  const input = document.getElementById(input);
-  form.style.display = "block";
-}
+  const form = document.getElementById('form-container');
+  const inputName = document.getElementById(input);
+  const inputScore = document.getElementById(input);
+  const btn = document.createElement('button');
+
+  inputName.id = 'name';
+  inputName.type = 'text';
+  inputName.placeholder = 'Add your Name';
+
+  inputScore.id = 'score';
+  inputScore.type = 'number';
+  inputScore.placeholder = 'Add your Score';
+
+  btn.innerHTML = 'Submit';
+  btn.classList.add('btn');
+  btn.id = 'submit';
+  btn.addEventListener('click', () => {
+    const name = inputName.value;
+    const score = inputScore.value;
+    const newPlayer = new Player(name, score);
+    newPlayer.addPlayer();
+    newPlayer.sortPlayers();
+    newPlayer.displayPlayers();
+  });
+
+  form.append(inputName);
+  form.append(inputScore);
+  form.append(btn);
+};
 
 export default displayForm;
