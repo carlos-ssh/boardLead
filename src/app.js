2ca652f131627e974ae77dbd9a8ce9d91bb46bb5
Carlos-SSH <carlos.anezeh@me.com>
2021-11-13 13:49:40 -0600

    Add list from api.

--- src/app.js
+++ src/app.js
@@ -1,63 +1,58 @@
 import './style.css';
-import displayForm from './components/form';
-import { getScore, addScore } from './components/scores';
-import liElement from './components/listElements';
+import LiElement from './components/listElements';
+import { getScores, addScores } from './components/scores';
 import form from './components/form';
 
-
 const displayData = () => {
-  const response = getScore();
-  const sl = document.querySelector('.score-result');
-  const data = [];
-  const liLoading = liElement('loading...');
+  const sl = document.getElementById('score-result');
+  const response = getScores();
+  let data = [];
+  const liLoading = LiElement({ user: 'Getting data from API', score: '' });
   sl.append(liLoading);
 
   response
-  .then((response) => {
-    data = [...response.data.result];
-    data.forEach((score) => {
-      const li = liElement(score);
-      sl.append(li);
-      console.log(score);
+    .then((response) => {
+      // handle success
+      data = [...response.data.result];
+      sl.innerHTML = '';
+      data.forEach((score) => {
+        const li = LiElement(score);
+        sl.append(li);
+      });
+    })
+    .catch((error) => {
+      const liLoading = LiElement({
+        user: "Couldn't get data from API :(",
+        score: '',
+      });
+      console.log(error);
+      sl.append(liLoading);
     });
-  })
-  .catch((err) => {
-    const liLoading = liElement({
-      user: 'No Response from api',
-      score: 'No Response from api'
-    });
-    
-    sl.append(liLoading);
-  });
 };
 
 const submitScore = (e) => {
   e.preventDefault();
   const user = document.getElementById('name').value;
   const score = document.getElementById('score').value;
-  const game = { user, score};
+  const game = { user, score };
 
-  const response = addScore(game);
+  const response = addScores(game);
   response.then((resp) => {
     console.log(resp);
-    const li = liElement(resp.data.result);
-    const sl = document.querySelector('.score-result');
-    sl.append(li);
   });
-
   displayData();
-}
-
-const handleRefresh = () => {
-  displayData();
-}
-
-const app = () => {
-  displayData();
-  form(submitScore);
-
-  const refresh = document.querySelector('.refresh');
-  refresh.addEventListener('click', handleRefresh);
 };
 
-app();
+const hanldeRefresh = () => {
+  displayData();
+};
+
+const startApp = () => {
+  displayData();
+  form(submitScore);
+  // Event haldler for refresh
+  const btnRefresh = document.getElementById('refresh');
+  btnRefresh.onclick = hanldeRefresh;
+};
+
+startApp();
--- src/components/scores.js
+++ src/components/scores.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 const gameId = 'Zl4d7IVkemOTTVg2fUdz';
 const apiURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
-// https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/
-const getScore = async () => {
+
+async function getScores() {
   const response = await axios.get(`${apiURL}/games/${gameId}/scores`);
   return response;
-};
+}
 
-const addScore = async (game) => {
+async function addScores(game) {
   const response = await axios.post(`${apiURL}/games/${gameId}/scores`, game);
   return response;
-};
+}
 
-export { getScore, addScore };
+export { getScores, addScores };
